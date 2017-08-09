ab52a9786c56038f1583a69c400503b85ac9d06e
pkindalov <panayotkindalov@gmail.com>
2017-08-09 13:44:01 +0300

    Added UsersService. Inject it to the constructor and used it in register method in register.component.ts

--- client/src/app/users/register.component.ts
+++ client/src/app/users/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { RegisterUser } from './register-user.model'
+import { UsersService } from './users.service'
 
 @Component({
     selector: 'register',
@@ -9,8 +10,12 @@ import { RegisterUser } from './register-user.model'
 export class RegisterComponent{
     user: RegisterUser = new RegisterUser()
 
+    constructor(private usersService:UsersService){}
 
     register(){
-        console.log(this.user)
+        this.usersService
+              .register(this.user)
+              .subscribe(res => console.log(res))
+            
     }
 }
