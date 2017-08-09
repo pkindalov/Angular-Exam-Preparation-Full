3a761bfceba94a355d5545be812e6d116770624b
pkindalov <panayotkindalov@gmail.com>
2017-08-09 18:32:08 +0300

    Updated handle errors

--- client/src/app/store/core/core.reducer.ts
+++ client/src/app/store/core/core.reducer.ts
@@ -6,7 +6,17 @@ export function coreReducer(state = initialState, action){
     const result = action.result
 
     if(result){
-        const message = result.message
+        let message = result.message
+        const errors = result.errors
+        if(errors){
+            const keys = Object.keys(errors)
+            if(keys.length > 0){
+            const firstKey = Object.keys(errors)[0]
+            message = errors[firstKey]
+            }
+        }
+
+
         if(message){
             return Object.assign({}, state, {
                 message
