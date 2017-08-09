0ea5d54641049df951c7d0b6dc40185d6557f304
pkindalov <panayotkindalov@gmail.com>
2017-08-09 22:55:11 +0300

    Added login link in navigation component. Updated routes in routes.module.ts. Added login component

--- client/src/app/routes.module.ts
+++ client/src/app/routes.module.ts
@@ -1,9 +1,11 @@
 import {  NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { RegisterComponent } from './users/register.component'
+import { LoginComponent } from './users/login-component'
 
 const routes: Routes = [
-    { path: 'users/register', component: RegisterComponent  }
+    { path: 'users/register', component: RegisterComponent  },
+    { path: 'users/login', component: LoginComponent }
 ]
 
 
