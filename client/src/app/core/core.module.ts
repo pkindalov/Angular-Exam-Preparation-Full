796c7d5c6e9f39b8331a2dc70ee8d51edbc28e45
pkindalov <panayotkindalov@gmail.com>
2017-08-09 18:21:41 +0300

    Updated core.module.ts Added Navbar component, MessageHandler Component and exports them

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,17 +4,24 @@ import { RouterModule } from '@angular/router'
 import {CommonModule } from '@angular/common'
 
 import { NavbarComponent } from './navbar.component'
+import { MessageHandlerComponent } from './message-handler.component'
 
 import { HttpService } from './http.service'
 
 @NgModule({
-    declarations: [NavbarComponent],
+    declarations: [
+        NavbarComponent,
+        MessageHandlerComponent
+    ],
     imports: [
         RouterModule,
         CommonModule
     ],
     providers: [HttpService],
-    exports: [NavbarComponent]
+    exports: [
+        NavbarComponent,
+        MessageHandlerComponent
+    ]
 })
 
 
