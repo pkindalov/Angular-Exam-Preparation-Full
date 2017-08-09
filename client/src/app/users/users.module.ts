8b76c792ed1d526f174119c4a7d456e94d9aa3e7
pkindalov <panayotkindalov@gmail.com>
2017-08-09 13:45:01 +0300

    Imported UsersService and added it to the providers in users.module.ts

--- client/src/app/users/users.module.ts
+++ client/src/app/users/users.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common'
 import { RegisterComponent } from './register.component'
+import { UsersService } from './users.service'
 
 @NgModule({
     declarations: [RegisterComponent],
+    providers: [UsersService],
     imports: [FormsModule]
 })
 
