638ff421ef77e391c098e88ed94d83cbbbced148
pkindalov <panayotkindalov@gmail.com>
2017-08-09 23:15:27 +0300

    Added Login component in users module

--- client/src/app/users/users.module.ts
+++ client/src/app/users/users.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common'
 import { RegisterComponent } from './register.component'
+import { LoginComponent } from './login-component'
 import { UsersService } from './users.service'
 import { UsersActions } from '../store/users/users.actions'
 
 @NgModule({
-    declarations: [RegisterComponent],
+    declarations: [
+        RegisterComponent,
+        LoginComponent
+    ],
     providers: [
         UsersService,
         UsersActions
