232ea511c276ee898be013c20827c580d8826097
pkindalov <panayotkindalov@gmail.com>
2017-08-09 16:40:28 +0300

    Updated users.module.ts. Added and imported UsersActions

--- client/src/app/users/users.module.ts
+++ client/src/app/users/users.module.ts
@@ -3,10 +3,14 @@ import { FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common'
 import { RegisterComponent } from './register.component'
 import { UsersService } from './users.service'
+import { UsersActions } from '../store/users/users.actions'
 
 @NgModule({
     declarations: [RegisterComponent],
-    providers: [UsersService],
+    providers: [
+        UsersService,
+        UsersActions
+    ],
     imports: [FormsModule]
 })
 
