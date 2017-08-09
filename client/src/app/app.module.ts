4f722b9805ca6947b50850260140f93d78077641
pkindalov <panayotkindalov@gmail.com>
2017-08-09 13:41:40 +0300

    Imported HttpModule and added to the imports in app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http'
 
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { CarRoutesModule } from './routes.module'
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     CarRoutesModule,
     CoreModule,
     UsersModule
