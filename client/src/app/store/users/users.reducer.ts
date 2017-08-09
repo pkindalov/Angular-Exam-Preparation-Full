c62b736ba032f331c24aa8063d609de83d5d29ad
pkindalov <panayotkindalov@gmail.com>
2017-08-09 16:37:41 +0300

    Updated file users.reducer.ts. Added reducer method to handle action type

--- client/src/app/store/users/users.reducer.ts
+++ client/src/app/store/users/users.reducer.ts
@@ -1,5 +1,23 @@
 import { initialState } from './users.state'
 
+import { USER_REGISTERED } from './users.actions'
+
+function userRegistration(state, action){
+    const result = action.result
+
+    return Object.assign({}, state, {
+        userRegistered: result.success
+    })
+}
+
 export function usersReducer(state = initialState, action){
-    return state
+
+    switch(action.type){
+        case USER_REGISTERED:
+            return userRegistration(state, action)
+        default:
+            return state    
+    }
+
+    
 }
