7a6735169bceefe330a0cf95ff302a91f8f268db
pkindalov <panayotkindalov@gmail.com>
2017-08-09 23:11:22 +0300

    Added method for login

--- client/src/app/store/users/users.reducer.ts
+++ client/src/app/store/users/users.reducer.ts
@@ -1,6 +1,9 @@
 import { initialState } from './users.state'
 
-import { USER_REGISTERED } from './users.actions'
+import {
+     USER_REGISTERED,
+     USER_LOGGED_IN
+     } from './users.actions'
 
 function userRegistration(state, action){
     const result = action.result
@@ -10,11 +13,25 @@ function userRegistration(state, action){
     })
 }
 
+function userLogin(state, action){
+    const result = action.result
+
+   return Object.assign({}, state, {
+            userAuthenticated: result.success,
+            token: result.token,
+            username: result.user ? result.user.name :  null 
+        })
+
+
+}
+
 export function usersReducer(state = initialState, action){
 
     switch(action.type){
         case USER_REGISTERED:
             return userRegistration(state, action)
+        case USER_LOGGED_IN:
+            return userLogin(state, action)    
         default:
             return state    
     }
