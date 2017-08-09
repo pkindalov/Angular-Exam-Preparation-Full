970598cf057adf6290710bca0eec79ac1d6a149f
pkindalov <panayotkindalov@gmail.com>
2017-08-09 22:56:14 +0300

    Edited core.reducer.ts file. Added new methods and refactoring olds of them

--- client/src/app/store/core/core.reducer.ts
+++ client/src/app/store/core/core.reducer.ts
@@ -1,28 +1,47 @@
 import { initialState} from './core.state'
+import { ROUTES_CHANGE} from './core.actions'
+
+
+function handleMessage(state, action){
+      const result = action.result
+
+        if(result){
+            let message = result.message
+            const errors = result.errors
+            if(errors){
+                const keys = Object.keys(errors)
+                if(keys.length > 0){
+                const firstKey = Object.keys(errors)[0]
+                message = errors[firstKey]
+                }
+            }
+
+
+            if(message){
+                return Object.assign({}, state, {
+                    message
+                })
+            }
+        }
+        return state
+}
+
+
+function routeChange(state, action){
+    return Object.assign({}, state, {
+        message: null
+    })
+}
+
 
 
 export function coreReducer(state = initialState, action){
 
-    const result = action.result
-
-    if(result){
-        let message = result.message
-        const errors = result.errors
-        if(errors){
-            const keys = Object.keys(errors)
-            if(keys.length > 0){
-            const firstKey = Object.keys(errors)[0]
-            message = errors[firstKey]
-            }
-        }
-
-
-        if(message){
-            return Object.assign({}, state, {
-                message
-            })
-        }
+    if(action.type === ROUTES_CHANGE){
+        return routeChange(state, action)
+    }else{
+        return handleMessage(state, action)
     }
 
-    return state
+    
 }
