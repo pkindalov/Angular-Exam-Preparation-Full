e6d883c06bc9a5ea3fb316f2e49e4159857fb19d
pkindalov <panayotkindalov@gmail.com>
2017-08-09 11:53:17 +0300

    Import in user module, forms module

--- client/src/app/users/users.module.ts
+++ client/src/app/users/users.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common'
 import { RegisterComponent } from './register.component'
 
-NgModule({
-    imports: [RegisterComponent]
+@NgModule({
+    declarations: [RegisterComponent],
+    imports: [FormsModule]
 })
 
 export class UsersModule{
