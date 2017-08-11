6197a862d3cb599277ac89e610299f43d531036f
pkindalov <panayotkindalov@gmail.com>
2017-08-11 15:02:26 +0300

    Added reducer file for stats in global reducer file - reducer.ts

--- client/src/app/store/reducer.ts
+++ client/src/app/store/reducer.ts
@@ -3,8 +3,10 @@ import { IAppState } from './app.state'
 
 import { coreReducer } from './core/core.reducer'
 import { usersReducer } from './users/users.reducer'
+import { statsReducer } from './stats/stats.reducer'
 
 export const reducer = combineReducers<IAppState>({
     core: coreReducer,
-    users: usersReducer
+    users: usersReducer,
+    stats: statsReducer
 })
