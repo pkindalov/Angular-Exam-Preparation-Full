80210100e6f3e20d3b349a86108ff618299dae0a
pkindalov <panayotkindalov@gmail.com>
2017-08-11 15:00:52 +0300

    Imported StatsComponent and added it to routes constant in global routes.module.ts file

--- client/src/app/routes.module.ts
+++ client/src/app/routes.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router'
 import { RegisterComponent } from './users/register.component'
 import { LoginComponent } from './users/login-component'
 import { AddCarComponent } from './cars/add-car.component'
+import { StatsComponent } from './stats/stats.component'
 import { PrivateRoute } from './core/private-route'
 
 const routes: Routes = [
+    { path: '', component: StatsComponent} ,
     { path: 'users/register', component: RegisterComponent  },
     { path: 'users/login', component: LoginComponent },
     {
@@ -13,6 +15,8 @@ const routes: Routes = [
          component: AddCarComponent,
           canActivate: [PrivateRoute]
      }
+      
+    
 ]
 
 
