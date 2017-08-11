750a9eea13e8821311e7af4e43bb9b3e0bf0c118
pkindalov <panayotkindalov@gmail.com>
2017-08-11 11:12:55 +0300

    Imported and added AddCarComponent and PrivateRoute in routes.module.ts

--- client/src/app/routes.module.ts
+++ client/src/app/routes.module.ts
@@ -2,10 +2,17 @@ import {  NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { RegisterComponent } from './users/register.component'
 import { LoginComponent } from './users/login-component'
+import { AddCarComponent } from './cars/add-car.component'
+import { PrivateRoute } from './core/private-route'
 
 const routes: Routes = [
     { path: 'users/register', component: RegisterComponent  },
-    { path: 'users/login', component: LoginComponent }
+    { path: 'users/login', component: LoginComponent },
+    {
+         path: 'cars/add', 
+         component: AddCarComponent,
+          canActivate: [PrivateRoute]
+     }
 ]
 
 
