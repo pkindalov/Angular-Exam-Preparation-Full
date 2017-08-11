6d42e5d18cc06f253c2932486319bed529b28cbc
pkindalov <panayotkindalov@gmail.com>
2017-08-11 09:44:43 +0300

    Imported new config.ts file with settings. Imported and injected AuthService

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http'
-import { Router, NavigationStart } from '@angular/router'
+import { Router } from '@angular/router'
 
 import { NgReduxModule, NgRedux } from 'ng2-redux'
 import { store, IAppState } from './store'
@@ -12,7 +12,11 @@ import { UsersModule } from './users/users.module'
 import { CoreModule } from './core/core.module'
 import { CarRoutesModule } from './routes.module'
 
-import { ROUTES_CHANGE } from './store/core/core.actions'
+import { AuthService } from './core/auth.service'
+
+import { config } from './core/config'
+// import { ROUTES_CHANGE } from './store/core/core.actions'
+// import { USER_LOGGED_IN } from './store/users/users.actions'
 
 @NgModule({
   declarations: [
@@ -33,18 +37,41 @@ export class AppModule {
 
   constructor(
     private ngRedux:NgRedux<IAppState>,
+    private authService: AuthService,
     private router: Router
   
   ){
     this.ngRedux.provideStore(store)
+    config(ngRedux, router, authService)
 
-    this.router.events.subscribe(ev => {
-      if( ev instanceof NavigationStart){
-        this.ngRedux.dispatch({
-          type: ROUTES_CHANGE
-        })
-      }
-    })
+    // this.router.events.subscribe(ev => {
+    //   if( ev instanceof NavigationStart){
+    //     this.ngRedux.dispatch({
+    //       type: ROUTES_CHANGE
+    //     })
+    //   }
+    // })
+
+
+    // if(this.authService.isUserAuthenticated()){
+    //     const token = this.authService.getToken()
+    //     const username  = this.authService.getUser()
+
+    //     this.ngRedux.dispatch({
+    //       type: USER_LOGGED_IN,
+    //       result: {
+    //         success: true,
+    //         token,
+    //         user: {
+    //           name: username
+    //         }
+    //       }
+
+    //     })
+
+    // }
+
+
   }
 
 }
