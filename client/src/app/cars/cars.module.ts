f43f3e43137239186e93b70e9fcf7645fe2ad027
pkindalov <panayotkindalov@gmail.com>
2017-08-12 09:54:23 +0300

    Updated cars.module.ts. Imported and added CommonModule, FormsModule, RouterModule, ListCarComponent, CarsActions, CarsService

--- client/src/app/cars/cars.module.ts
+++ client/src/app/cars/cars.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 
 import { AddCarComponent } from './add-car.component'
+import { ListCarComponent } from './list-cars.component'
+import { CarsActions } from '../store/cars/cars.actions'
+import { CarsService } from './cars.service'
 
 
 @NgModule({
-    declarations: [AddCarComponent]
+    declarations: [
+        AddCarComponent,
+        ListCarComponent
+    ],
+    providers: [
+        CarsActions,
+        CarsService
+    ],
+    imports: [
+        FormsModule,
+        CommonModule,
+        RouterModule
+    ]
 })
 
 export class CarsModule{
