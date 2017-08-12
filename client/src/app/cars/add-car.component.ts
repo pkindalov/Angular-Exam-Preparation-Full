31cc39771cf056c9b50884742b8f2e0ba9b94c7c
pkindalov <panayotkindalov@gmail.com>
2017-08-12 09:52:17 +0300

    Updated file add-car.component.ts - Added object car, inject in constructor carsActions, ngRedux,router. Added method addCar()

--- client/src/app/cars/add-car.component.ts
+++ client/src/app/cars/add-car.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core'
+import { AddCarModel } from './add-car.model'
+import { CarsActions } from '../store/cars/cars.actions'
+import { NgRedux } from 'ng2-redux'
+import { IAppState } from '../store/app.state'
+import { Router } from '@angular/router'
 
 
 @Component({
@@ -7,5 +12,28 @@ import { Component } from '@angular/core'
 })
 
 export class AddCarComponent{
+    car: AddCarModel = new AddCarModel();
+
+    constructor(
+        private carsActions: CarsActions,
+        private ngRedux: NgRedux<IAppState>,
+        private router: Router
+    ){}
+
+
+    addCar(){
+        this.carsActions
+                .addCar(this.car)
+              
+        this.ngRedux
+                .select(state => state.cars)        
+                .subscribe(cars => {
+                    if(cars.carAdded){
+                        const carId = cars.carAddedId
+                        this.router.navigateByUrl(`/cars/details/${carId}`)
+                    }
+                })
+                
+    }
 
 }
