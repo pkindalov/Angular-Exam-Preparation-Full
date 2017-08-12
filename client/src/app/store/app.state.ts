2bafd5ac7e4b8bb1842fd260ef00c099caefec77
pkindalov <panayotkindalov@gmail.com>
2017-08-12 10:00:21 +0300

    Added new local state for cars in global state app.state.ts

--- client/src/app/store/app.state.ts
+++ client/src/app/store/app.state.ts
@@ -1,9 +1,11 @@
 import { ICoreState } from './core/core.state'
 import { IUserState } from './users/users.state'
 import { IStatsState } from './stats/stats.state'
+import { ICarsState } from './cars/cars.state'
 
 export interface IAppState{
     core: ICoreState,
     users: IUserState,
-    stats: IStatsState
+    stats: IStatsState,
+    cars: ICarsState
 }
