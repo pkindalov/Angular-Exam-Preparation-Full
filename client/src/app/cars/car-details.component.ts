7458b29b4c3f3b15aece81919afec786bdf3e8f0
pkindalov <panayotkindalov@gmail.com>
2017-08-12 11:48:08 +0300

    Added car component - ts and html files

---
+++ client/src/app/cars/car-details.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { CarsActions } from '../store/cars/cars.actions'
+
+import { NgRedux } from 'ng2-redux'
+import { IAppState } from '../store/app.state'
+
+
+@Component({
+    selector: 'car-details',
+    templateUrl: './car-details.component.html'
+})
+
+export class CarDetailsComponent implements OnInit{
+
+    car: object = {}
+
+    constructor(
+       private route: ActivatedRoute,
+       private carsActions: CarsActions,
+       private ngRedux: NgRedux<IAppState>
+    ){}
+
+
+    ngOnInit(){
+        this.route.params
+                .subscribe(params => {
+                    const id = params['id']
+                   
+                    this.carsActions.details(id)
+                    this.ngRedux.select(state => state.cars.carDetails)
+                                             .subscribe(car => {
+                                                this.car = car
+                                             })
+                })
+    }
+
+
+}
